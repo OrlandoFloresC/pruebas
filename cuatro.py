3b3731cf5a39759de2c73a068f98593172f7acb0
OrlandoFloresC <hardestpart94@hotmail.com>
2024-02-24 23:03:18 -0600

    Primer commit

---
+++ cuatro.py
@@ -0,0 +1,31 @@
+#video #7
+#operadores relacionales
+# establecen relacion entre dos valores, para compararlos
+#tienen el mismo niovel en su evaluacion
+#tienen menor prioridad que los aritmeticos
+
+# > mayor que
+#< menor que
+# >= mayor o igual que
+# <= menor o igual que
+# != diferete
+# == igual
+
+a = 10
+b = 20
+c = 30
+
+resultado = a+b == c
+
+resultado = 10 < 20
+print(resultado)
+resultado = 10 > 20
+print(resultado)
+resultado = 10 == 20
+print(resultado)
+resultado = 10 != 20
+print(resultado)
+resultado = 10 <= 20
+print(resultado)
+resultado = 10 >= 20
+print(resultado)
