8dc0f801b26310bacdcdd85cdccbb4a7fb2402db
OrlandoFloresC <hardestpart94@hotmail.com>
2024-02-25 12:13:44 -0600

    ejercicio 1

---
+++ diez.py
@@ -0,0 +1,13 @@
+#video #13 programacion ats
+
+#capitulo 1, elementos basicos
+
+#ejercicio # 1: escriba la siguiente expresion en forma de expresion algoritmica
+#((a**3)((b**2)- 2ac)) / 2b
+
+a = float(input("Digite un numero: "))
+b = float(input("Digite un numero: "))
+c = float(input("Digite un numero: "))
+resultado = (a**3 * (b**2 - 2*a*c))/(2 * b)
+print (resultado)
+
