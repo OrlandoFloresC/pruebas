b8fc088f3e0715dd9db6463fd963185dead8de3a
OrlandoFloresC <hardestpart94@hotmail.com>
2024-02-25 13:51:30 -0600

    ejercicio 4

---
+++ trece.py
@@ -0,0 +1,12 @@
+#video #15 programacion ats
+
+#capitulo 1, elementos basicos
+
+#ejercicio # 4: hacer un programa que que con el radio de un circulo nos de su
+#area y longitud de circunferencia
+import math
+r =  50 #float(input("Ingresa el radio de tu circulo: "))
+area = math.pi * r**2
+longitud = 2 * math.pi * r
+print(f"El area es {area:.2f}")
+print (f"La longitud es {longitud:.2f}")
