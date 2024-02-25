09d09dfc53e7eee32f40cd7de6aed547fdfe0f3f
OrlandoFloresC <hardestpart94@hotmail.com>
2024-02-25 14:07:27 -0600

    ejercicio  5

---
+++ catorce.py
@@ -0,0 +1,11 @@
+#video #16 programacion ats
+
+#capitulo 1, elementos basicos
+
+#ejercicio # 5: una tienda ofrece un descuento de 15 % sobre el total de la compra
+# y un cluente desea saber cuanto debera pagar finalmente por su compra
+
+c = 100#float(input("Total de compra antes de descuento: "))
+restar = c/100 *15
+total = c - restar
+print (f"EL TOTAL DESPUES DEL DESCUENTO ES {total:.2f}")
