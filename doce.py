a655aaef5d3c7c1a7712f7f04870abb2c3f88226
OrlandoFloresC <hardestpart94@hotmail.com>
2024-02-25 13:22:21 -0600

    ejercicio 3

---
+++ doce.py
@@ -0,0 +1,16 @@
+#video #14 programacion ats
+
+#capitulo 1, elementos basicos
+
+#ejercicio # 3 hacer un programa para intercambiar el valor de dos variables
+print("Este programa intercambia dos valores asignados a ,b")
+a = float(input("Escribe un valor a: "))
+b = float(input("Escribe un valor b: "))
+
+#a = 10
+#b = 5
+x = a
+a = b
+b = x
+print("a =",a,"b =",b)
+print ("Si ves los valores intercambiados\nEntonces el programa funciona")
