b0f6f553db3d4b62acab996cd5707b6d069dec2a
OrlandoFloresC <hardestpart94@hotmail.com>
2024-02-25 14:42:46 -0600

    capitulo 2

---
+++ uno_cap2.py
@@ -0,0 +1,14 @@
+#video #18 programacion ats
+
+#capitulo 2, condicionales
+
+#sirve para comparra 2 valores como verdadero o falso
+
+numero = 0#float(input("Digite un numero: "))
+
+if numero > 0:
+    print("El numero es positivo")
+elif numero == 0:
+    print("El numero es cero")
+else:
+    print("el numero es negativo")
