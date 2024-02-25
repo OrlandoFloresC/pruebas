7a6f331930467fdef6bbb858ee7b22d18a32667f
OrlandoFloresC <hardestpart94@hotmail.com>
2024-02-25 11:39:54 -0600

    primer commit

---
+++ nueve.py
@@ -0,0 +1,32 @@
+#video #12 rpgramacion ats
+
+#funciones integradas
+
+n = str(10) #entero a string
+print (n)
+
+n = str(10.8) #float a string
+print (n)
+
+n = bin(10) #entero a binario
+print (n)
+
+n = hex(10) #entero a hexadecimal
+print (n)
+
+n = int("0b1010",2) #binario a entero, el 2 es la base 2
+print (n)
+
+n = int("0xa",16) #hexadecimal a entero, el 16 es la base 16
+print (n)
+
+#----------------otras rfuncionmes integradas----
+
+n = abs(-18) #valor absoluto
+print (n)
+
+n = round(5.6) #redondeo, siempre a lo mas cerca
+print (n)
+
+n = len("oralndo") #arroja el numero de acaracteres 
+print (n)
