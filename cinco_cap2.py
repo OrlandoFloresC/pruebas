538b19a6b521023d7591f2ff3c018c96ceadfc6f
OrlandoFloresC <hardestpart94@hotmail.com>
2024-02-25 16:05:57 -0600

    ejercicio 8

---
+++ cinco_cap2.py
@@ -0,0 +1,11 @@
+#video #21 programacion ats
+
+#capitulo 2, condicionales
+
+# ejercicio 8: hacer un programa que pida un caracter e indique si es una vocal o no
+
+letra = "A" # input("Introduce una letra: ")
+if letra.lower() == ("a" or "e" or "i" or "o" or "u"):
+    print("La letra es una vocal")
+else:
+    print("La letra no es una vocal")
