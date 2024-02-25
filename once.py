7ec6a39a4449f9dd78cd66826392dd5947a96c78
OrlandoFloresC <hardestpart94@hotmail.com>
2024-02-25 12:26:14 -0600

    ejercicio  2

---
+++ once.py
@@ -0,0 +1,16 @@
+#video #13 programacion ats
+
+#capitulo 1, elementos basicos
+
+#ejercicio # 2: determinar la operacion logica de la siguiente operacion
+
+#((3+5*8)<3 and ((-3/2)*4+2<2))or(a>b)
+#a = float(input("a = "))
+#b = float(input("b = "))
+
+a = 2
+b = 3
+
+
+resultado = ((3+5*8)<3 and ((-3/2)*4+2<2))or(a>b)
+print (resultado)
