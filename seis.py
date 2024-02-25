68fba79d75f3bb5e9db3828ba5a28810252e5a30
OrlandoFloresC <hardestpart94@hotmail.com>
2024-02-25 10:42:22 -0600

    primer commit

---
+++ seis.py
@@ -0,0 +1,13 @@
+#video #9
+
+#operadores de asignacion
+# ejemplo
+
+a = 0 #aqui siempre haber creado la variable
+
+#lo de aqui abajo es correcto
+a = a + 5
+#pero esto igua es correcto
+a += 5   #y esto se puede hacer para las opraciones basica, acorta el codigo
+
+print(a)
