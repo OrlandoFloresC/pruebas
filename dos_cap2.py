b17f05e6015ecb439d2f96317c3a44302bb8f54d
OrlandoFloresC <hardestpart94@hotmail.com>
2024-02-25 15:07:44 -0600

    capitulo 2

---
+++ dos_cap2.py
@@ -0,0 +1,17 @@
+#video #19 programacion ats
+
+#capitulo 2, condicionales combinados
+edad = 101 #int(input("Digite su edad: "))
+
+if edad > 0 and edad <=100:
+    print("Edad correcta")
+    if edad >= 18:
+        print("Es mayor de edad")
+    else:
+        print("No es mayor de edad")
+else:
+    print("Edad incorrecta")
+
+
+
+
