113264c7f4f80e8131b4407df0429442fbd93ac6
OrlandoFloresC <hardestpart94@hotmail.com>
2024-02-25 10:56:37 -0600

    primer commit

---
+++ siete.py
@@ -0,0 +1,11 @@
+#video #10
+
+#salida de datos por consola
+
+nombre = "orlando"
+edad = 29
+print ("hola",nombre,"tienes",edad,"años")
+
+print("Hola {}, tienes {} años".format(nombre,edad))
+
+print (f"hola {nombre}, tienes {edad} años")
