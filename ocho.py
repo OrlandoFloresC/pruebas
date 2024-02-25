2086d552c929296e8c94159ff67c7805186cfcb1
OrlandoFloresC <hardestpart94@hotmail.com>
2024-02-25 11:10:39 -0600

    Primer commit

---
+++ ocho.py
@@ -0,0 +1,13 @@
+#video #11
+
+#entrada de datos
+
+nombre = input("Digita tu nombre: ")  #datos de tipo cadena
+
+print (f"hola {nombre}")
+
+numero = int(input("Digita un numero: "))  #datos de tipo entero
+print (numero + 10)
+
+numero = float(input("Digita un numero: "))  #datos de tipo flotante
+print (numero + 12.7)
