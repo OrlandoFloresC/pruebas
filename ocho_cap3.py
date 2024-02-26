c9d06f0373059786dc01316f1dc207db1a7a873e
OrlandoFloresC <hardestpart94@hotmail.com>
2024-02-25 21:57:51 -0600

    capitulo 3

---
+++ ocho_cap3.py
@@ -0,0 +1,21 @@
+#video #32 programacion ats
+
+#capitulo 3: pilas (ultimo en entrar, primero en salir)
+pila = [1,2,3]
+
+#agregando elemntos por el final
+pila.append(4)
+pila.append(5)
+print(pila)
+#cacando elemntos por el final
+pila.pop()
+print(pila)
+
+n = pila.pop()
+print(f"Sacando el elemnto {n}")
+print(pila)
+
+n = pila.pop()
+print(f"Sacando el elemnto {n}")
+print(pila)
+
