0660271e9640cc395369791c99110b84693a1b99
OrlandoFloresC <hardestpart94@hotmail.com>
2024-02-25 23:08:00 -0600

    capitulo 4

---
+++ dos_cap4.py
@@ -0,0 +1,10 @@
+#video #38 programacion ats
+
+#capitulo 4: bucles , for : con numero determinado 
+
+for i in [1,2,3,4,5,6,7,8,9, 10]:
+    print(i)
+
+coleccion = [1,2,3,4,5]
+for i in coleccion:
+    print("hola")
