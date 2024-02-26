92a3d94e03aa6406f8596df4b77d196f00ee3a33
OrlandoFloresC <hardestpart94@hotmail.com>
2024-02-25 19:17:52 -0600

    capitulo 3

---
+++ uno_cap3.py
@@ -0,0 +1,13 @@
+#video #25 programacion ats
+
+#capitulo 3, listas (colecciones)
+#son parecidas a los arreglos en otros lenguajes
+
+lista = [] # esto es una lista vacia
+list = ["lunes", "martes", "miercoles","jueves","viernes",40,34.7,[1,3,3,5], True]
+print(lista)
+print (list[2])
+print (list[-1])
+print(list[0:3])
+print(list)
+#print (list[7]) #esto es un error, esta fuera de rango, ya que no existe el indice 7
