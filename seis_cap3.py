6df77377ed898b21502c0ea5dff74378139adff7
OrlandoFloresC <hardestpart94@hotmail.com>
2024-02-25 21:30:51 -0600

    capitulo 3

---
+++ seis_cap3.py
@@ -0,0 +1,15 @@
+#video #30 programacion ats
+
+#capitulo 3: diccionarios, los elemantos se guardan desordenandps
+#se guardan dos elemntos por ´posicion, clave y valor, solo debe haber una clave
+
+diccionario = {"azul":"blue", "rojo":"red", "verde":"green"} #"clave":"valor"
+print(diccionario["azul"]) #NOS MUESTRA UN VALOR ESPECIFICO, POR MEDIO DE LA CLAVE
+diccionario["amarillo"] = "yellow"  #agregamos un valor a nuestro diccionario
+print(diccionario)
+diccionario["amarillo"] = "YELLOW" #cambiamos a mayusculas
+print(diccionario)
+del(diccionario["amarillo"])
+print(diccionario)
+#hay otras cosas
+
