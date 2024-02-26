64ced4aad372d66d0f54eca22552d67982862396
OrlandoFloresC <hardestpart94@hotmail.com>
2024-02-25 22:52:35 -0600

    capitulo 4

---
+++ uno_cap4.py
@@ -0,0 +1,21 @@
+#video #37 programacion ats
+
+#capitulo 4: bucles #bucle while (numero indeterminado de iteraciones)
+
+import math
+numero = int(input("Dijite un numero: "))
+while numero < 0:
+    print("Ha ocurrudo un error, el valor debe ser positivo")
+    numero = int(input("Dijite un numero: "))
+print(f"\nSu raiz cuadrada es: {(math.sqrt(numero)):.2f}")
+
+i = 0
+while i<10:
+    print("hola mundo")
+    i = i + 1
+
+i = 0
+while i<10:
+    print(i)
+    i = i + 1
+
