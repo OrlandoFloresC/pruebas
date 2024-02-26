7b7fb2555f0f7aeaa415de8024640d5beeab6ca1
OrlandoFloresC <hardestpart94@hotmail.com>
2024-02-25 19:52:47 -0600

    capitulo 3

---
+++ tres_cap3.py
@@ -0,0 +1,14 @@
+#video #27 programacion ats
+
+#capitulo 3, tuplas (otro tipo de coleccion), son inmutables. no se puede añadir nada
+
+tupla = (4,4,[2,3,4,5,], "Hola")
+
+#puedo hacer cualquier operacion, menos las que lo modifican
+print(tupla[1]) #si nos permite mostrar determianbdo indice
+
+lista = list(tupla) #asi convertimos en lista
+print (lista)
+
+tupla = tuple(lista) # asi conviertes en tupla
+print(tupla)
